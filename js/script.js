25d3c60e382e6dcf09df1056ed1718e762e1cb12
Manjeet <m4sharma8860@gmail.com>
2023-04-11 11:47:38 +0530

    I added some functionality

--- js/script.js
+++ js/script.js
@@ -4,3 +4,29 @@
 
 
 
+
+function downloadPDF() {
+
+    // Replace the URL below with the URL of your PDF file
+    const pdfUrl = 'docs/Resume--Manjeet.pdf';
+
+    // Create an invisible link element
+    const link = document.createElement('a');
+    link.style.display = 'none';
+    document.body.appendChild(link);
+
+    // Set the link's href and download attributes
+    link.href = pdfUrl;
+    link.setAttribute('download', 'manjeet-resume.pdf');
+
+    // Simulate a click on the link to start the download
+    link.click();
+
+    // Clean up
+    document.body.removeChild(link);
+}
+
+
+
+
+
